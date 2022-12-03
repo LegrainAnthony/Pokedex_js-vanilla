c56a04a5f08459f9ae428d4c20b5495ddca971b8
Anthony Legrain <legrain.anthony51@gmail.com>
2022-12-03 20:46:03 +0100

    exo get a number for fetching

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-function fetchPokemon () {
+const contenaire = document.querySelector('#contenair') 
 
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
-      .then((response) => response.json())
-      .then((data) => data.results );
-
-}
-
-let test = fetchPokemon();
-
-console.log(test);
-
-const pokemons = ['pikachu', 'bulbizard', 'poussifeu', 'bulbizar', "tiplouf", 'florentin', 'salameche'];
-const chifres = ['1', '2', '3', '4', "5", '6', '7'];
-// Récupéré la div contenairb=u
-
-const divContenair = document.querySelector("#contenair");
-
-// créer une fonction qui va créer des paragraphe en lui ajoutant du texte.
 
 function addParagraphe(value){
-    let newP = document.createElement('p');//crée une balise <p>
+    let newP = document.createElement('p');
     newP.textContent = value;
     return newP;
 }
 
-// pour chaque élément du tableaux ajouté le paragraphe au contenaire.
-
-for(let i = 0; i < pokemons.length; i++){
-    let paragraph = addParagraphe(pokemons[i]);
-    paragraph.classList.add('main_title')
-    divContenair.append(paragraph);
-}
-
-for(let i = 0; i < chifres.length; i++){
-    let paragraph = addParagraphe(i);
-    divContenair.append(paragraph);
+function fetchPokemon (number) {
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${number}&offset=0`)
+    .then((response) => response.json())
+    .then((data) => {
+        data.results.map(pokemon => {
+            let paragraph = addParagraphe(pokemon.name);
+            paragraph.classList.add('main_title')
+            divContenair.append(paragraph);
+        })
+    });
+    
 }
