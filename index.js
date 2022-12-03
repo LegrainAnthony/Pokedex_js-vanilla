3dba6435b5ef465c18ea9b69441039c45dd6c0c3
XtrEmZerGtxt <117867152+XtrEmZerGtxt@users.noreply.github.com>
2022-12-03 14:26:16 +0100

    first test commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
+git status : sert a voir les fichier modifier
+
+git add . : ajoute tous les fichié à un paquet // crée un packet
+
+git commit -m "nom du commit"//a chaque fin de tache a faire (sauvegarde en cas de merde(skyrim)) //nom du packet
+
+git push . : envoie le code sur github
+
+*/
+
 /* 
 * j'initie un tableaux avec des pokemons à l'intérieur qui s'apelle pokemons
 * je séléctionne la div dans laquelle je vais vouloir injécté mes pokémons
@@ -40,4 +51,3 @@ pokemons.map(pokemon => {
 //     const newELM = addElement(pokemons[i]);
 //     contenair.appendChild(newELM)
 // }
-console.log("test re")
