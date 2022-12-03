7994c3dbed93732044e032c0ae70efbba7c750a0
Anthony Legrain <legrain.anthony51@gmail.com>
2022-12-03 20:18:12 +0100

    fetch bug

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+function fetchPokemon () {
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+      .then((response) => response.json())
+      .then((data) => data.results );
+
+}
+
+let test = fetchPokemon();
+
+console.log(test);
+
 const pokemons = ['pikachu', 'bulbizard', 'poussifeu', 'bulbizar', "tiplouf", 'florentin', 'salameche'];
 const chifres = ['1', '2', '3', '4', "5", '6', '7'];
-// Récupéré la div contenair.
+// Récupéré la div contenairb=u
 
 const divContenair = document.querySelector("#contenair");
 
