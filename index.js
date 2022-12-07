333840926664d1694fe37a225862a98f335feb59
Anthony Legrain <legrain.anthony51@gmail.com>
2022-12-07 20:25:29 +0100

    traduction name done and types need to fix

--- index.js
+++ index.js
@@ -1,24 +1,98 @@
 const divContenair = document.querySelector('#main_contenair');
+const formulaireLanguage = document.querySelector('#header__lang_form');
+const buttonLanguage = document.querySelectorAll('.heander__langPokedex');
+const button = document.querySelector('#test')
+const pokemonCards = document.querySelectorAll('.main__card_contenair')
+
+formulaireLanguage.addEventListener('click', hundleSubmit)
+
 
 function hundleSubmit(event) {
     event.preventDefault();
 }
 
-function addLink () {
-    let newLink = document.createElement('a');
-    return newLink
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
 }
 
-
-function addNewDiv () {
-    let newDiv = document.createElement('div');
-    return newDiv
+function hundleClickButtonLanguage () {
+    for(let button of buttonLanguage) {
+        button.addEventListener('click' , () => {
+            fetchLanguage(button.value);
+        });
+    }
 }
 
-function addParagraphe(value){
-    let newP = document.createElement('p');
-    newP.textContent = value;
-    return newP;
+function fetchLanguage(value) {
+    removeElementsByClass('main__card_contenair')
+    switch (value) {
+        case 'fr':
+            fetchPokemon(4);
+            
+            break;
+            
+            case 'en':
+                
+                fetchPokemon(8);
+                
+                break;
+                
+                case 'ja':
+                    
+            fetchPokemon(9);
+                        
+            break;
+            
+            case 'ko':
+                
+                fetchPokemon(2);
+                
+                break;
+                
+                case 'de':
+                    
+                    fetchPokemon(5);
+                    
+                    break;
+                    
+                    case 'es':
+                        
+                        fetchPokemon(6);
+                        
+                        break;
+                        
+                        case 'it':
+                            
+                            fetchPokemon(7);
+                            
+                            break;
+                            
+                            case 'zh':
+                                
+                                fetchPokemon(3);
+                                
+                                break;
+                            }
+                        }
+                        
+                        function addLink () {
+                            let newLink = document.createElement('a');
+                            return newLink
+                        }
+                        
+                        
+                        function addNewDiv () {
+                            let newDiv = document.createElement('div');
+                            return newDiv
+                        }
+                        
+                        function addParagraphe(value){
+                            let newP = document.createElement('p');
+                            newP.textContent = value;
+                            return newP;
 }
 
 function addImage(url) {
@@ -27,66 +101,63 @@ function addImage(url) {
     return newImage
 }
 
-function fetchPokemon () {
-    const result = document.querySelector('#header_input').value;
-    
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${151}&offset=0`)
+function fetchPokemon (value) { 
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`)
     .then((response) => response.json())
-    .then((data) => {
-        data.results.map(pokemon => {
+    .then((pokemons) => {
+        pokemons.results.map(pokemon => {
             fetch(pokemon.url)
             .then((response) => response.json())
-            .then((data) => {
-
-                let link = addLink();
-                link.href=pokemon.url;
-                link.classList.add('link__color')
-
-                let newDiv = addNewDiv();
-
-                let paragraph = addParagraphe(pokemon.name);
-                paragraph.classList.add('main__card__title');
-
-                let image = addImage(data.sprites.front_default);
-                image.classList.add('main__card__image');
-
-                let divTypeContenair = addNewDiv();
-                divTypeContenair.classList.add('main__contenair__type');
-
-                data.types.map(typeOfPokemon =>{
-                    //console.log(typeOfPokemon);
-                    let pokeType = addParagraphe(typeOfPokemon.type.name);
-                    pokeType.classList.add(`${typeOfPokemon.type.name}_color`);
+            .then((pokemonsDetails) => {
+                fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonsDetails.id}/`)
+                .then(response => response.json())
+                .then(pokemonsSpeciesDetails => {
                     
-                    divTypeContenair.append(pokeType)
+                    
+                    let link = addLink();
+                    link.href=pokemon.url;
+                    link.classList.add('link__color')
+                    
+                    
+                    let newDiv = addNewDiv();
+                    
+                    
+                    let paragraph = addParagraphe(pokemonsSpeciesDetails.names[value].name);
+                    paragraph.classList.add('main__card__title');
+                    
+                    let image = addImage(pokemonsDetails.sprites.front_default);
+                    image.classList.add('main__card__image');
+                    
+                    let divTypeContenair = addNewDiv();
+                    divTypeContenair.classList.add('main__contenair__type');
+                    
+                    pokemonsDetails.types.map(typeOfPokemon =>{
+                        fetch(typeOfPokemon.type.url)
+                        .then(response => response.json())
+                        .then((typeOfPokemonDetails) => {
+                            let pokeType = addParagraphe(typeOfPokemonDetails.names[value].name);
+                            pokeType.classList.add(`${typeOfPokemon.type.name}_color`);
+                            
+                            divTypeContenair.append(pokeType)
+                        })
+                    })
+                    
+                    newDiv.append(image);
+                    newDiv.append(paragraph);
+                    newDiv.append(divTypeContenair);
+                    newDiv.classList.add("main__card_contenair");
+                    link.append(newDiv)
+                    divContenair.append(link);
+                    
                 })
                 
-
-                newDiv.append(image);
-                newDiv.append(paragraph);
-                newDiv.append(divTypeContenair);
-                newDiv.classList.add("main__card_contenair");
-                link.append(newDiv)
-                divContenair.append(link);
-
+                
                 
             })
         })
-
+        
     });
 }
 
-fetchPokemon();
-
-function langFecth(){
-    let test = FR.name;
-    let retest = parseInt(test);
-    let i =0;
-    //console.log(retest);
-                FR.onclick = () => { 
-                    fetchPokemon();
-                }
-            }
-                //fetch(`"https://pokeapi.co/api/v2/language/${langfr}/"`)
-            
-langFecth();
+fetchPokemon(8);
+hundleClickButtonLanguage();
